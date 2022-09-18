662a54dd3244cf4e491b2cb71fceb8ebb43569aa
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-09-18 13:28:29 +0300

    add setProfile action

--- src/s1-main/m2-bll/reducers/profile-reducer.ts
+++ src/s1-main/m2-bll/reducers/profile-reducer.ts
@@ -1,7 +1,37 @@
-const initialState = {}
+const initialState = {
+		profile: null,
+}
 
-export const profileReducer = (state: any = initialState, action: any): any => {
-    switch (action.type){
-        default: return state
-    }
+export const profileReducer = (state: ProfileStateType = initialState, action: ActionsType): ProfileStateType => {
+		switch (action.type) {
+				case 'PROFILE/SET-PROFILE':
+						return {...state, profile: action.profile}
+				default:
+						return {...state}
+		}
+}
+//actions
+export const setProfile = (profile: ProfileType) =>
+		({type: 'PROFILE/SET-PROFILE', profile} as const)
+//thunks
+
+//types
+type ProfileStateType = {
+		profile: ProfileType | null
+}
+type ActionsType =
+		| SetProfileActionType
+export type SetProfileActionType = ReturnType<typeof setProfile>
+type ProfileType = {
+		_id: string
+		email: string
+		name: string
+		avatar?: string
+		publicCardPacksCount: number
+		created: Date
+		updated: Date
+		isAdmin: boolean
+		verified: boolean
+		rememberMe: boolean;
+		error?: string;
 }
