2a6eb924d8716e9f0905660bf422ec0384631735
Mart7anova <mart7anova7@gmail.com>
2022-09-19 14:16:12 +0300

    add checkEmail

---
+++ src/s2-features/f1-auth/a5-checkEmail/CheckEmail.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+import styleContainer from '../../../s1-main/m1-ui/common/c2-styles/Container.module.css';
+import style from './CheckEmail.module.scss';
+import styleBlock from '../../../s1-main/m1-ui/common/c2-styles/Block.module.css';
+
+import emailImg from '../../../s1-main/m1-ui/common/c3-image/photo/email.png'
+import {Button} from '../../../s1-main/m1-ui/common/c1-components/Button/Button';
+
+import {Navigate} from 'react-router-dom';
+import {login} from '../../../s1-main/m1-ui/u1-Route/Variables/routeVariables';
+
+export const CheckEmail = () => {
+    const [linkToLogin, setLinkToLogin] = useState(false)
+
+    const onBackToLoginClick = () => {
+        setLinkToLogin(true)
+    }
+
+    if(linkToLogin){
+        return <Navigate to={login}/>
+    }
+
+    return (
+        <div className={`${styleContainer.container} ${style.checkEmailContainer}`}>
+            <div className={`${styleBlock.block} ${style.checkEmailPassBlock}`}>
+                <h1 className={style.header}>
+                    Check Email
+                </h1>
+                <img src={emailImg} alt={'email'}/>
+                <span className={style.informationText}>
+                    We've sent an Email with instruction to {'props.email'}
+                </span>
+                <Button onClick={onBackToLoginClick}>Back to login</Button>
+            </div>
+        </div>
+    );
+};
