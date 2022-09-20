086f4376f5d14927b42b0b610a73c7a7cfb6bf09
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-09-21 00:33:36 +0300

    remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './s1-main/m1-ui/App';
+import {App} from 's1-main/m1-ui/App';
 import reportWebVitals from './reportWebVitals';
-import {store} from './s1-main/m2-bll/store';
+import {store} from 's1-main/m2-bll/store';
 import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-		<React.StrictMode>
 				<Provider store={store}>
 						<App/>
 				</Provider>
-		</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
