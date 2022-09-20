803e1a07f03ffed7c686602299fc56c423bc1f3b
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-09-20 14:24:19 +0300

    add navlink to forgotPassword

--- src/s2-features/f1-auth/a1-login/Login.tsx
+++ src/s2-features/f1-auth/a1-login/Login.tsx
@@ -3,11 +3,11 @@ import s from './Login.module.scss'
 import {Input} from '../../../s1-main/m1-ui/common/c1-components/Input/Input';
 import {Checkbox} from '../../../s1-main/m1-ui/common/c1-components/Checkbox/Checkbox';
 import {Button} from '../../../s1-main/m1-ui/common/c1-components/Button/Button';
-import {Navigate} from 'react-router-dom';
+import {Link, Navigate} from 'react-router-dom';
 import {useFormik} from 'formik';
 import {useSelector} from 'react-redux';
 import {AppRootStateType, useAppDispatch} from '../../../s1-main/m2-bll/store';
-import {profile} from '../../../s1-main/m1-ui/u1-Route/Variables/routeVariables';
+import {forgotPassword, profile} from '../../../s1-main/m1-ui/u1-Route/Variables/routeVariables';
 import {loginThunk} from '../../../s1-main/m2-bll/reducers/auth-reducer';
 
 
@@ -38,7 +38,9 @@ export const Login = () => {
 										<span>Password</span>
 										<Input type="password" {...formik.getFieldProps('password')}/>
 										<Checkbox {...formik.getFieldProps('rememberMe')}>Remember me</Checkbox>
-										<span>Forgot Password?</span>
+										<Link to={forgotPassword}>
+												<span>Forgot Password?</span>
+										</Link>
 										<Button>Sign In</Button>
 										<span>Already have an account?</span>
 										<span className={s.signUpButton}>Sign Up</span>
