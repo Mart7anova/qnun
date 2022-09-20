d24dcf14544920a1313fd24ff1f3794cfe3a7ef8
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-09-20 10:35:49 +0300

    add spinner component

---
+++ src/assets/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Spinner.module.scss'
+
+export const Spinner = () => {
+		return (
+				<svg className={s.spinner} viewBox="0 0 50 50">
+						<circle className={s.path} cx="25" cy="25" r="20" fill="none" strokeWidth="5"/>
+				</svg>
+		);
+};
