0c76127d011fcd97157b325ec68a861c0e35fb09
Roma <elusiuve15.09.93@icloud.com>
2022-09-22 16:04:30 +0300

    PageNotFound edit

--- src/s1-main/m1-ui/PageNotFound/PageNotFound.tsx
+++ src/s1-main/m1-ui/PageNotFound/PageNotFound.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './PageNotFound.module.scss'
-import { showComponents} from '../u1-Route/Variables/routeVariables';
+import {login} from '../u1-Route/Variables/routeVariables';
 import {Link} from "react-router-dom";
 
 export const PageNotFound = () => {
@@ -8,7 +8,7 @@ export const PageNotFound = () => {
         <div className={style.pageContainer}>
             <h1>Error 404</h1>
             <h2>Page not found</h2>
-            <span>Go <Link to={showComponents}>home</Link></span>
+            <span>Go <Link to={login}>home</Link></span>
         </div>
     );
 };
