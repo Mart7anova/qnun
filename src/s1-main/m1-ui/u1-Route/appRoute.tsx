3a784e78b2893def235f205f5d975555feb26d5d
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-09-24 18:36:12 +0300

    add new route for CardsPage

--- src/s1-main/m1-ui/u1-Route/appRoute.tsx
+++ src/s1-main/m1-ui/u1-Route/appRoute.tsx
@@ -8,26 +8,28 @@ import {NewPassword} from '../../../s2-features/f1-auth/a4-newPassword/NewPasswo
 import {Profile} from '../../../s2-features/f2-profile/Profile';
 import {PATH} from './Variables/routeVariables';
 import {Main} from '../Main';
-import {PacksList} from '../../../s2-features/f3-packsList/PacksList';
+import {PacksPage} from 's2-features/f3-packsList/PacksPage';
 import {CheckEmail} from '../../../s2-features/f1-auth/a5-checkEmail/CheckEmail';
+import {CardsPage} from 's2-features/f3-packsList/cardsList/CardsPage';
 
 export const AppRoute = () => {
 
-    return (
-        <Routes>
-            <Route path={PATH.PACKS_LIST} element={<Main/>}>
-                <Route path={PATH.OTHER_ROUTS} element={<Navigate to={PATH.PAGE_NOT_FOUND}/>}/>
-                <Route path={PATH.PAGE_NOT_FOUND} element={<PageNotFound/>}/>
+		return (
+				<Routes>
+						<Route path={PATH.PACKS_LIST} element={<Main/>}>
+								<Route path={PATH.OTHER_ROUTS} element={<Navigate to={PATH.PAGE_NOT_FOUND}/>}/>
+								<Route path={PATH.PAGE_NOT_FOUND} element={<PageNotFound/>}/>
 
-                <Route path={PATH.LOGIN} element={<Login/>}/>
-                <Route path={PATH.REGISTRATION} element={<Registration/>}/>
-                <Route path={PATH.FORGOT_PASSWORD} element={<ForgotPassword/>}/>
-                <Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
+								<Route path={PATH.LOGIN} element={<Login/>}/>
+								<Route path={PATH.REGISTRATION} element={<Registration/>}/>
+								<Route path={PATH.FORGOT_PASSWORD} element={<ForgotPassword/>}/>
+								<Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
 
-                <Route index element={<PacksList/>}/>
-                <Route path={PATH.PROFILE} element={<Profile/>}/>
-                <Route path={PATH.CHECK_EMAIL} element={<CheckEmail/>}/>
-            </Route>
-        </Routes>
-    );
+								<Route index element={<PacksPage/>}/>
+								<Route path={`pack/:id`} element={<CardsPage/>}/>
+								<Route path={PATH.PROFILE} element={<Profile/>}/>
+								<Route path={PATH.CHECK_EMAIL} element={<CheckEmail/>}/>
+						</Route>
+				</Routes>
+		);
 };
