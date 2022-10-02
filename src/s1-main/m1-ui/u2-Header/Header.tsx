31676bafb5f2bc5855a4b4ca1066e651bfdcaaaa
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-10-02 10:18:16 +0300

    fix linearprogressbar posistion

--- src/s1-main/m1-ui/u2-Header/Header.tsx
+++ src/s1-main/m1-ui/u2-Header/Header.tsx
@@ -8,43 +8,44 @@ import {useAppSelector} from 's1-main/m2-bll/store';
 import noAvatar from '../../../assets/no-avatar.png'
 import {getIsLoggedIn} from 's1-main/m2-bll/selectors/auth-selectors';
 import {getProfileInfo} from 's1-main/m2-bll/selectors/profile-selectors';
-import {appStatus} from "../../m2-bll/selectors/app-selectors";
-import {LinearProgress} from "@mui/material";
+import {appStatus} from '../../m2-bll/selectors/app-selectors';
+import {LinearProgress} from '@mui/material';
 
 export const Header = () => {
-    const {pathname} = useLocation()
-    const isLoggedIn = useAppSelector(getIsLoggedIn)
-    const profile = useAppSelector(getProfileInfo)
-    const status = useAppSelector(appStatus)
+		const {pathname} = useLocation()
+		const isLoggedIn = useAppSelector(getIsLoggedIn)
+		const profile = useAppSelector(getProfileInfo)
+		const status = useAppSelector(appStatus)
 
-    return (
-        <header className={s.header}>
-            <div className={s.container}>
-                <Link to={PATH.PACKS_LIST}>
-                    <img src={logo} alt="logo"/>
-                </Link>
-                {isLoggedIn
-                    ? (
-                        <Link to={PATH.PROFILE} style={{textDecoration: 'none', color: 'black'}}>
-                            <div className={s.profileContainer}>
-                                <div className={s.profileName}>
-                                    {profile.name}
-                                </div>
-                                <img
-                                    src={profile.avatar || noAvatar}
-                                    alt="ava" style={{width: '36px', height: '36px', borderRadius: '50%'}}/>
-                            </div>
-                        </Link>
-                    )
-                    : (
-                        pathname !== PATH.LOGIN &&
-                        <Link to={PATH.LOGIN}>
-                            <Button style={{width: '115px'}}>Sign in</Button>
-                        </Link>
-                    )
-                }
-            </div>
-            {status === "loading" && <LinearProgress color="primary"/>}
-        </header>
-    );
+		return (
+				<header className={s.header}>
+						<div className={s.container}>
+								<Link to={PATH.PACKS_LIST}>
+										<img src={logo} alt="logo"/>
+								</Link>
+								{isLoggedIn
+										? (
+												<Link to={PATH.PROFILE} style={{textDecoration: 'none', color: 'black'}}>
+														<div className={s.profileContainer}>
+																<div className={s.profileName}>
+																		{profile.name}
+																</div>
+																<img
+																		src={profile.avatar || noAvatar}
+																		alt="ava" style={{width: '36px', height: '36px', borderRadius: '50%'}}/>
+														</div>
+												</Link>
+										)
+										: (
+												pathname !== PATH.LOGIN &&
+												<Link to={PATH.LOGIN}>
+														<Button style={{width: '115px'}}>Sign in</Button>
+												</Link>
+										)
+								}
+						</div>
+						{status === 'loading' &&
+								<LinearProgress className={s.linearProgress} color="primary" />}
+				</header>
+		);
 };
